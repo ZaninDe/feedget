016814267416ea10084c5c278b06a2feeae85263
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-13 19:49:18 -0300

    front and backend integrated

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,5 +1,5 @@
-import { MailAdapter } from "../adapters/mail-adapter";
-import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+import { MailAdapter } from '../adapters/mail-adapter';
+import { FeedbacksRepository } from '../repositories/feedbacks-repository';
 
 interface SubmitFeedbackUseCaseRequest {
   type: string;
@@ -17,15 +17,15 @@ export class SubmitFeedbackUseCase {
     const { type, comment, screenshot } = request;
 
     if (!type) {
-      throw new Error("Type is required.");
+      throw new Error('Type is required.');
     }
 
     if (!comment) {
-      throw new Error("comment is required.");
+      throw new Error('comment is required.');
     }
 
-    if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
-      throw new Error("Invalid screenshot format.");
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.');
     }
 
     await this.feedbacksRepository.create({
@@ -35,13 +35,14 @@ export class SubmitFeedbackUseCase {
     });
 
     await this.mailAdapter.sendMail({
-      subject: "New Feedback",
+      subject: 'New Feedback',
       body: [
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Type of feedback: ${type}</p>`,
         `<p>Comment: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}">` : ``,
         `</div>`,
-      ].join("\n"),
+      ].join('\n'),
     });
   }
 }
--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -13,16 +15,26 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypesInfo = feedbackTypes[feedbackType]
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
-      screenshot,
-      comment,
-    })
 
+    setIsSendingFeedback(true)
+
+    // console.log({
+    //   screenshot,
+    //   comment,
+    // })
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot,
+    })
+    setIsSendingFeedback(false)
     onFeedbackSent();
   }
 
@@ -34,7 +46,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
           className="top-4 left-4 absolute text-zinc-400 hover:text-zinc-100"
           onClick={onFeedbackRestartRequested}
         >
-          <ArrowLeft weight="bold" className="w-4 h-4"/>
+          <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
 
         <span className="text-xl leading-6 flex items-center gap-2">
@@ -42,7 +54,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
           {feedbackTypesInfo.title}
         </span>
 
-          <CloseButton />
+        <CloseButton />
       </header>
 
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
@@ -59,10 +71,10 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
           />
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Send feedback
+            {isSendingFeedback ? <Loading /> : 'Send feedback'}
           </button>
         </footer>
 
